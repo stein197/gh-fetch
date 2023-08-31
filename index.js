401f71e76f7cacb4617b5125c54f4178670eb484
Nail Gafarov <nil20122013@gmail.com>
2023-08-31 14:19:59 +0500

    Added data_fetch()

--- index.js
+++ index.js
@@ -19,6 +19,7 @@
 
 // @ts-check
 
+const API_HOST = "https://api.github.com";
 const Type = {
 	Repo: "repo",
 	Gist: "gist"
@@ -71,4 +72,32 @@ function options_check(opts) {
  */
 function app_create(root, opts, logger) {
 	return {root, opts, logger};
+}
+
+/**
+ * @param {string} endpoint
+ * @param {string} user
+ * @param {string} auth
+ * @returns {Promise}
+ */
+async function data_fetch(endpoint, user, auth) {
+	let page = 1;
+	let result = [];
+	while (true) {
+		const response = await fetch(API_HOST + endpoint + "?page=" + page, {
+			headers: {
+				Authorization: `token ${auth}`
+			}
+		});
+		if (response.status >= 400)
+			throw new Error(`Failed to fetch data for user ${user} at path ${endpoint}. Status: ${response.status}`);
+		const data = await response.json();
+		if (!Array.isArray(data))
+			throw new Error(`Expected to be array. Actual value: ${JSON.stringify(data)}`);
+		result.push(...data);
+		if (!data.length)
+			break;
+		page++;
+	}
+	return result;
 }
