f6f9a97a4b54ba6ddc89be0e63a425e1190696ea
Nail Gafarov <nil20122013@gmail.com>
2023-09-02 12:06:30 +0500

    Added time measure

--- bin.js
+++ bin.js
@@ -7,17 +7,22 @@ const colors = require("colors");
 const arg = require("@stein197/cli/arg");
 const index = require(".");
 
+const MS_SECOND = 1000;
+
 (async function main(...args) {
 	const data = arg.parse(args);
 	const [type] = data.args;
 	const logger = logger_create();
 	const root = process.cwd();
-	try {
-		await index(root, type, data.opts, logger);
-	} catch (e) {
-		logger.error(e.message);
-		process.exit(1);
-	}
+	const ms = await time_measure(async () => {
+		try {
+			await index(root, type, data.opts, logger);
+		} catch (e) {
+			logger.error(e.message);
+			process.exit(1);
+		}
+	});
+	logger.info(`Time elapsed: ${ms / MS_SECOND}s`);
 })(...process.argv.slice(2));
 
 function logger_create() {
@@ -26,4 +31,13 @@ function logger_create() {
 		success: msg => console.log(colors.green(`[SUCCESS]: ${msg}`)),
 		error: msg => console.error(colors.red(`[ERROR]: ${msg}`))
 	};
+}
+
+/**
+ * @param {() => Promise} f
+ * @returns {Promise<number>}
+ */
+function time_measure(f) {
+	const tmStart = Date.now();
+	return f().then(() => Date.now() - tmStart);
 }
